d7b56d0fb666dd287188101347d907af0ae2fde3
saeedd77 <33974679+saeedd77@users.noreply.github.com>
2020-12-05 21:11:43 +0530

    Add files via upload
    Added Test cases using REST Template

--- rest-client-demo/src/test/java/com/saeed/assignment/restclientdemo/RestClientDemoApplicationTests.java
+++ rest-client-demo/src/test/java/com/saeed/assignment/restclientdemo/RestClientDemoApplicationTests.java
@@ -5,6 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 class RestClientDemoApplicationTests {
@@ -17,11 +24,38 @@ class RestClientDemoApplicationTests {
 	}
 
 	@Test
-	final void testGetBookJSON() {
+	final void testGetBookByIDJSON() {
 		String urlStr = "http://localhost:8080/book/4";
 		String str = Quotation.getBooksJSON(urlStr);
 		String gsonStr = "{\"bookid\":4,\"bookname\":\"Java\",\"author\":\"Saeed\",\"price\":350}";
 		assertEquals(gsonStr, str, "JSON Not Matched.");
+	}
+
+	@Test
+	final void testGetBooksUsingTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		String urlStr = "http://localhost:8080/books";
+		ResponseEntity<String> response = restTemplate.getForEntity(urlStr, String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode(), "Rest Get not success.");
+	}
+
+	@Test
+	final void testGetBookByIDUsingTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		String urlStr = "http://localhost:8080/book/4";
+		ResponseEntity<String> response = restTemplate.getForEntity(urlStr, String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode(), "Rest Get By ID not success.");
+
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root;
+		try {
+			root = mapper.readTree(response.getBody());
+			JsonNode name = root.path("name");
+			assertNotNull(name.asText());
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
